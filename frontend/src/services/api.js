d08e52e0520eb0a441e182d319792bd19d33dec6
Sarthak893 <kumarsarthak741@gmail.com>
2026-01-09 15:06:04 +0530

    backend deployed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+});
 
 API.interceptors.request.use((req) => {
   // ✅ Check BOTH — in case token exists in either
